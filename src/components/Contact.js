0eb5bdd86293628303827362c8795f3d05f48546
aryoung5 <aryoung5@wisc.edu>
2021-01-09 21:26:53 -0600

    First commit

---
+++ src/components/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <div className="row section-head">
+        <div className="ten columns">
+          <p className="lead">
+            Please contact us at{' '}
+            <a href="mailto:wisconsinautnomous@studentorg.wisc.edu">
+              <strong>wisconsinautnomous@studentorg.wisc.edu</strong>
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
---
+++ src/orgData.js
@@ -0,0 +1,43 @@
+let orgData = {
+  imagebaseurl: 'https://WisconsinAutonomous.github.io/',
+  name: 'Wisconsin Autonomous',
+  role: 'student organization at the University of Wisconsin - Madison',
+  linkedIn: 'wisconsin-autonomous',
+  roleDescription:
+    'We are focused on the development of autonomous vehicle technologies. Our primary goal is to compete in the Indy Autonomous Challenge and the evGrand Prix.',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/company/wisconsin-autonomous/',
+      className: 'fa fa-linkedin',
+    },
+    {
+      name: 'github',
+      url: 'http://github.com/WisconsinAutonomous',
+      className: 'fa fa-github',
+    },
+    {
+      name: 'twitter',
+      url: 'http://twitter.com/https://twitter.com/wisconsinsae?lang=en',
+      className: 'fa fa-twitter',
+    },
+    {
+      name: 'facebook',
+      url: 'https://www.facebook.com/wisconsinautonomous/',
+      className: 'fa fa-facebook',
+    },
+  ],
+  aboutSite:
+    'This website was constructed for the primary use to display documentation for our members. Please see links to documentation or repositories below.',
+  documentation: [
+    {
+      documentationName: 'WA Simulator Documentation',
+      documentationLink: 'https://wisconsinautonomous.github.io/wa_simulator',
+      repoLink: 'https://github.com/WisconsinAutonomous/wa_simulator',
+      documentationDescription:
+        'Documentation for our main simulator environment. Under construction...',
+    },
+  ],
+}
+
+export default orgData
